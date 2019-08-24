1dede8f06adcefbb1bd84341aa71ac4807aeb1c2
John Guest <jcguest90@gmail.com>
2019-08-24 07:49:48 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,3 +2,22 @@
 def first_challenge
   epic_tragedy = {:montague => {}, :capulet => {}}
 end
+
+    
+def second_challenge
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+  }
+  
+end
